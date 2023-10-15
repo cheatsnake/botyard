93595972935df3e4f10ea11ca5e2f3d8b06ba57a
cheatsnake <72691412+cheatsnake@users.noreply.github.com>
2023-10-15 13:24:12 +0300

    update settings

--- web/src/modules/settings/settings-modal.tsx
+++ web/src/modules/settings/settings-modal.tsx
@@ -1,10 +1,10 @@
-import { Modal, ActionIcon, useMantineColorScheme, Text, Divider, Flex, Select, ColorScheme } from "@mantine/core";
+import { Modal, ActionIcon, useMantineColorScheme, Text, Flex, Switch, Tabs, Kbd, Box } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { IconSettings } from "@tabler/icons-react";
+import { useState } from "react";
 
 export const SettingsModal = () => {
     const [opened, { open, close }] = useDisclosure(false);
-    const { colorScheme, toggleColorScheme } = useMantineColorScheme();
 
     return (
         <>
@@ -19,24 +19,8 @@ export const SettingsModal = () => {
                     </Text>
                 }
             >
-                <Divider />
-                <Modal.Body mih={"25vh"}>
-                    <Flex py="lg" align="center" justify="space-between">
-                        <Text>Global theme</Text>
-                        <Select
-                            maw={120}
-                            placeholder="Pick one"
-                            defaultValue={colorScheme}
-                            data={[
-                                { value: "dark", label: "Dark" },
-                                { value: "light", label: "Light" },
-                            ]}
-                            dropdownPosition="bottom"
-                            onChange={(event) => {
-                                toggleColorScheme(event as ColorScheme);
-                            }}
-                        />
-                    </Flex>
+                <Modal.Body mih={"25vh"} p={0}>
+                    <SettingsTabs />
                 </Modal.Body>
             </Modal>
 
@@ -44,5 +28,70 @@ export const SettingsModal = () => {
                 <IconSettings />
             </ActionIcon>
         </>
+    );
+};
+
+const SettingsTabs = () => {
+    return (
+        <Tabs defaultValue="appearance" p={0}>
+            <Tabs.List>
+                <Tabs.Tab value="appearance">Appearance</Tabs.Tab>
+                <Tabs.Tab value="shortcuts">Shortcuts</Tabs.Tab>
+            </Tabs.List>
+
+            <Box px={window.innerWidth > 500 ? "sm" : 0}>
+                <AppearanceTab />
+                <ShortcutsTab />
+            </Box>
+        </Tabs>
+    );
+};
+
+const AppearanceTab = () => {
+    const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+    const [isDark, setIsDark] = useState(colorScheme === "dark");
+
+    return (
+        <Tabs.Panel value="appearance" pt="md">
+            <Flex justify="space-between">
+                <Text>Enable dark theme</Text>
+
+                <Switch
+                    width="100%"
+                    labelPosition="left"
+                    size="md"
+                    checked={isDark}
+                    onChange={(event) => {
+                        const val = event.currentTarget.checked;
+                        toggleColorScheme(val ? "dark" : "light");
+                        setIsDark(val);
+                    }}
+                />
+            </Flex>
+        </Tabs.Panel>
+    );
+};
+
+const SHORTCUTS = [
+    { label: "Show bot commands", key1: "Ctrl", key2: "K" },
+    { label: "Copy last bot message", key1: "Ctrl", key2: "B" },
+    { label: "Focus message textarea", key1: "Ctrl", key2: "M" },
+    { label: "Toggle theme", key1: "Ctrl", key2: "J" },
+];
+
+const ShortcutsTab = () => {
+    return (
+        <Tabs.Panel value="shortcuts" pt="md">
+            <Flex direction="column" gap="md">
+                {SHORTCUTS.map((sc) => (
+                    <Flex gap="lg" key={sc.label}>
+                        <Box>
+                            <Kbd>{sc.key1}</Kbd> + <Kbd>{sc.key2}</Kbd>
+                        </Box>
+                        <Text>{sc.label}</Text>
+                    </Flex>
+                ))}
+            </Flex>
+        </Tabs.Panel>
     );
 };
