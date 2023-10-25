4e4ceb749147e706d4c4b31a585b2c0c27653832
cheatsnake <72691412+cheatsnake@users.noreply.github.com>
2023-10-25 20:42:30 +0300

    fix type

--- web/src/helpers/debounce.ts
+++ web/src/helpers/debounce.ts
@@ -1,5 +1,5 @@
 export function debounce<T extends unknown[]>(func: (...args: T) => void, delay = 1000): (...args: T) => void {
-    let timer: number | null = null;
+    let timer: NodeJS.Timeout | null = null;
     return (...args: T) => {
         if (timer) clearTimeout(timer);
         timer = setTimeout(() => {
